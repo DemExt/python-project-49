cf46ab516b5c3baa188acb137ec5c1388fb34e72
DemExt <xldemlx@gmail.com>
2025-04-23 23:59:22 +0300

    restruct package

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,23 @@
+#from .brain_games import welcome_user
+
+def welcome_user():
+    name = ''
+    while name == '':
+        print('May I have your name? ', end='')
+        name = input()
+    print('Hello, ', name, '!', sep='')
+
 def greet():    
     print('Welcome to the Brain Games!')
+    welcome_user()
 
 def main():
     greet()
+    
 
 if __name__ == "__main__":
     main()
+    
+
+
 
